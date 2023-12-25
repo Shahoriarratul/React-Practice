27ad4dc44ca853bfad87bd9f58cac4f0db2c507a
Ratul <shahoriar.ratul@gmail.com>
2023-12-25 12:36:44 +0600

    new project

---
+++ worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../Components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Welcome to our website!</h1>
+    </div>
+  );
+}
+
+export default Homepage;
