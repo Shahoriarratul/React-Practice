7a4b75231aaf12cd711338a6019c219a6b57c241
Ratul <shahoriar.ratul@gmail.com>
2024-01-18 03:04:20 +0600

    Refactor codebase and add authentication (generated by blackbox.ai )

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../Components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../Components/Map";
+import User from "../Components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
