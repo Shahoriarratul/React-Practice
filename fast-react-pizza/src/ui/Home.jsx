2bdffa0f9df70c245e145e7aebd3012c89bd5570
Ratul <shahoriar.ratul@gmail.com>
2024-01-30 03:26:01 +0600

    Update files for fast-react-pizza project. (generated by blackbox.ai )

--- fast-react-pizza/src/ui/Home.jsx
+++ fast-react-pizza/src/ui/Home.jsx
@@ -1,15 +1,15 @@
 function Home() {
-  return (
-    <div>
-      <h1 className="text-xl  font-semibold text-stone-700  text-center">
-        The best pizza.
-        <br />
-        <span className=" text-yellow-500">
-          Straight out of the oven, straight to you.
-        </span>
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center text-xl font-semibold">
+                The best pizza.
+                <br />
+                <span className=" text-yellow-500">
+                    Straight out of the oven, straight to you.
+                </span>
+            </h1>
+        </div>
+    )
 }
 
-export default Home;
+export default Home
