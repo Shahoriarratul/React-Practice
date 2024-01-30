ab4dad385e488f342ca13d2ea962db76d3ea2520
Ratul <shahoriar.ratul@gmail.com>
2024-01-31 01:13:53 +0600

    update 7 files

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -89,7 +89,10 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button disabled={isSubmitting}>
+                    <button
+                        disabled={isSubmitting}
+                        className=" inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed "
+                    >
                         {isSubmitting ? 'Placing Order...' : 'Order now'}
                     </button>
                 </div>
@@ -112,8 +115,9 @@ export async function action({ request }) {
             'Please a  give us your correct Phone number. we might need it to contract you'
     if (Object.keys(errors).length > 0) return errors
 
-    const newOrder = await createOrder(order)
+    // const newOrder = await createOrder(order)
 
-    return redirect(`/order/${newOrder.id}`)
+    // return redirect(`/order/${newOrder.id}`)
+    return null
 }
 export default CreateOrder
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom'
 import SearchOrder from '../features/order/searchOrder'
+import Username from '../features/user/Username'
 
 function Header() {
     return (
-        <header className="bg-yellow-500">
+        <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
             <Link to="/" className="tracking-widest">
                 Fast React Pizza Co.
             </Link>
             <SearchOrder />
-            <p> Ratul </p>
+            <Username />
         </header>
     )
 }
