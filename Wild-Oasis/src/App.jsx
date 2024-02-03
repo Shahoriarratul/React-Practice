1fc9b2ad58c1eaa002b402c3deff130e452a800b
Ratul <shahoriar.ratul@gmail.com>
2024-02-03 23:34:26 +0600

    update App.jsx

--- Wild-Oasis/src/App.jsx
+++ Wild-Oasis/src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from './pages/Dashboard'
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <BrowserRouter>
       <Routes path="dashboard">
-        <Route path="dashboard" element= {<Dashboard/> }/>
-        <Route path="dashboard" element= {<Dashboard/> }/>
-        <Route path="dashboard" element= {<Dashboard/> }/>
-        <Route path="dashboard" element= {<Dashboard/> }/>
-        <Route path="dashboard" element= {<Dashboard/> }/>
-
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
